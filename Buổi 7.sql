2047e6ff887ac76b04a274e57ce202b873c3f1f2
NguyenVietChauNgoc <nguyenvietchaungoc@gmail.com>
2024-06-25 11:11:21 +0700

    From ex1 to ex4

--- Buổi 7.sql
+++ Buổi 7.sql
@@ -8,4 +8,19 @@ select
 user_id
 CONCAT(UPPER(left(name,1)),substring(name,2))
 from Users
+EX4
+select 
+extract('month'from submit_date) as month,
+product_id
+round(avg(star,2)) as avg_star
+from reviews
+group by month,product_id
+order by month,product_id
 EX3
+SELECT manufacturer,
+  CONCAT('$', ROUND(SUM(total_sales) / 1000000), ' million') AS total_sales_million
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY SUM(total_sales) DESC, manufacturer;
+
+
