102b53673dd29450f2469b43228aa9d2dde621e5
NguyenVietChauNgoc <nguyenvietchaungoc@gmail.com>
2024-06-25 11:36:09 +0700

    Update Buổi 7.sql

--- Buổi 7.sql
+++ Buổi 7.sql
@@ -22,6 +22,35 @@ SELECT manufacturer,
 FROM pharmacy_sales
 GROUP BY manufacturer
 ORDER BY SUM(total_sales) DESC, manufacturer;
+EX5 
+select 
+sent_date,
+count(message_id)as message_count
+from messages 
+where extract(month from sent_date)=8 and extract (year from sent_date)= 2022
+group by sent_date
+order by message_count DESC
+Limit 2
+EX6 
+select 
+tweet_id
+from Tweets
+where length(content)>15
+EX8
+SELECT COUNT(employee_id) AS employee_count  
+FROM employees
+WHERE EXTRACT(MONTH FROM joining_date) BETWEEN 1 AND 7
+  AND EXTRACT(YEAR FROM joining_date) = 2022;
+EX9 
+select 
+position ('a'in first_name ) as position 
+from worker 
+where first_name ='Amitah'
+EX10
+select 
+Substring (title,length(winery)+2,4)
+from winemag_p2
+where country = ' Macedonia'
 
 
 
